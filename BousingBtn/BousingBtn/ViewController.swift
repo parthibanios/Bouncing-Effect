e637e568a4fc6caec069058a2dad5fc9bebb1945
varadarasu <purushoththaman@evapps.in>
2017-06-08 18:36:56 +0530

    Remove Comments

--- BousingBtn/BousingBtn/ViewController.swift
+++ BousingBtn/BousingBtn/ViewController.swift
@@ -17,29 +17,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
        workoutBtn.layer.cornerRadius = workoutBtn.frame.size.height/2
         workoutBtn.layer.borderWidth = 4 //2
-//        workoutBtn.layer.borderColor = UIColor.fromRGB(rgb: 0x00a6bb).cgColor
         workoutBtn.layer.borderColor = UIColor.red.cgColor
-
         halo = PulsingHaloLayer()
-        
-        //workoutBtn.layer.addSublayer(halo)
         workoutBtn.superview?.layer.insertSublayer(halo, below: workoutBtn.layer)
-        //halo.radius = 250.0
         halo.start()
-        //halo.backgroundColor = (UIColor.red).cgColor
-        //var layer = PulsingHaloLayer.init(layer: layer)//(PulsingHaloLayer, layer)
-//        PulsingHaloLayer *layer = [PulsingHaloLayer layer];
-//        self.halo = layer;
-//        [self.beaconView.superview.layer insertSublayer:self.halo below:self.beaconView.layer];
-//        
-//        [self setupInitialValues];
-//        
-//        [self.halo start];
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewDidLayoutSubviews() {
